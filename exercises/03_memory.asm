4ce442d16bc4989f6c18815d4e8f2ad4c09d0d2c
Thibma <balsamothibault@hotmail.fr>
2021-04-13 20:24:08 +0200

    finished exercices

--- exercises/03_memory.asm
+++ exercises/03_memory.asm
@@ -7,11 +7,17 @@
 # Questions:
 #
 # - What is the address of `num0`,` num1`, `result`?
+#	num0: 0x10010000, num1: 0x10010004, result: 0x10010008
 # - The last code address of your program
+# 	0x10010008
 # - How is the instruction quoted in machine language (hexadecimal): `lw t1, 0​​(t0)`
+#	Valuer de T0 stockée dans t1
 # - Is memory in the processor?
+#	Data : In the RAM, Text: In the processor
 # - When you access the memory by 32-bit words, what's the step?
+# 	la: pour récupérer l'adresse de la mémoire, lw: pour récupérer la valeur de cette adresse
 # - What is the value in memory of the value at address `num0`
+# 	0x0000007c
 
 # Imagine a C program like this: (available in 03_memory.c)
 #
@@ -48,20 +54,23 @@ result_g: .word 0
 main:
 
 # ??? <- @ num0
-l? ???, num0_g
+la t0, num0_g
 # load word from address 0 + register in register t1
-lw t1, 0 ​​(t0)
+lw t1, 0 (t0)
 
 # t2 <- @ num1
-l? t2, ???
+la t2, num1_g
 # Which instruction is used to load a memory word?
-??? t3, 0 (t2)
+lw t3, 0 (t2)
 
 # loading of the address of the result.
-la t4, ???
+la t4, result_g
 # make the addition between our two registers in one register.
-add ???, ???, ???
+add t5, t1, t3
 # store word, store the contents of the register in the address at 0 + t4
-sw ???, 0 (t4)
+sw t5, 0 (t4)
 
 # To be done by you totally, display our two numbers in the console (hint: syscall printInt)
+add a0, t5, zero
+li a7, 1
+ecall
--- exercises/04_array.asm
+++ exercises/04_array.asm
@@ -7,18 +7,26 @@
 #
 # Questions:
 #
+
 # - What does the lw: load word instruction do?
+#   Lw permet de stocker les valeurs d'une adresse dans un registre
+
 # - lw: What is the immediate use in front of the register containing the address?
+#   Le chiffre permet de retrouver l'élément du tableau que l'on souhaite en multipliant par 4 (0 = 0, 1 = 4, 2 = 8, etc)
+
 # - What will lw's destination register contain?
+#   La valeur de l'adresse
 #
 # - With an instruction like add what do the three registers correspond to?
 # `add reg0, reg1, reg2`
-# - reg0:
-# - reg1:
-# - reg2:
+# - reg0: Result
+# - reg1: Left operande
+# - reg2: Right operande
 #
 # - `la` what is it for?
+#	Recuperer une adresse d'une donnée de .data
 # - When working on 32-bit words the addresses are allocated from how many to how many?
+# From 0x00000000 to 0x7fffffff
 
 .data
 # /!\ Warning! they are global variables in memory! /!\
@@ -50,14 +58,16 @@ la t0, array_g # t0 contains the address of the array
 # To sum the array we will add manually,
 # the content of all the cells of the table.
 
-lw t1, 0​​(t0) # t1: array_g [0]
-# lw ???, 4 (???) <- TODO: To be completed!
+lw t1, 0(t0) # t1: array_g [0]
+lw t2, 4(t0) # <- TODO: To be completed!
 # You can also add on the address which is in t0
 add a0, t1, t2 # sum = t2 + t1
 
 
 # Here you have to add a0 which contains the sum
 # with the register which will contain the value of the 3rd cell of the table.
+lw t3, 8(t0)
+add a0, a0, t3
 
 ###
 # Teacher part: test if it works and displays a message accordingly.
